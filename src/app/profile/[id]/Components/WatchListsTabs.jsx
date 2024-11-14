ccdcceabe1da6bb4edd51a50de3ea574324310a7
Abhay Lodhi <abhaylodhiab11@gmail.com>
2024-11-14 12:28:29 +0530

    force pushing

--- src/app/profile/[id]/Components/WatchListsTabs.jsx
+++ src/app/profile/[id]/Components/WatchListsTabs.jsx
@@ -1,20 +1,77 @@
-import { Constant_Var_starterWatchLists_recent } from '@/utils/constants';
-import React from 'react'
+import {
+  Constant_Var_starterWatchLists_recent,
+  starterWatchLists,
+} from "@/utils/constants";
+import React, { useEffect, useState } from "react";
 
-const WatchListsTabs = ({WatchLists}) => {
-    
+const WatchListsTabs = ({
+  StarterWatchLists,
+  CustomWatchLists,
+  setSelectedWatchList,
+  selectedWatchList,
+}) => {
+  const [selectedCustomOption, setSelectedCustomOption] = useState(null);
+
+  const handleSelectChange = (event) => {
+    const selectedId = event.target.value;
+    const selectedObject = CustomWatchLists.find(
+      (ele) => ele.id === selectedId
+    );
+    setSelectedCustomOption(selectedObject);
+    setSelectedWatchList(selectedObject);
+  };
+
   return (
-    <div className='flex flex-row'>
-        {WatchLists.map((ele, ind) => {
+    <div className="flex flex-row">
+      {StarterWatchLists.map((ele, ind) => {
         return (
-          !(
-            ele.watchListName === Constant_Var_starterWatchLists_recent &&
-            ele.isSpecialStarter
-          ) && <div key={ind} className="mx-5">{ele.watchListName}</div>
+          <div
+            key={ind}
+            className={`mx-5 ${
+              selectedWatchList.id == ele.id && "bg-gray-600"
+            } p-2 rounded-md cursor-pointer`}
+            onClick={() => {
+              setSelectedWatchList(ele), setSelectedCustomOption(null);
+            }}
+          >
+            {ele.watchListName}
+          </div>
         );
       })}
-      </div>
-  )
-}
+      {StarterWatchLists.length == 0 ? (
+        CustomWatchLists.map((ele, ind) => {
+          return (
+            <div
+              key={ind}
+              className={`mx-5 ${
+                selectedWatchList.id == ele.id && "bg-gray-600"
+              } p-2 rounded-md cursor-pointer`}
+              onClick={() => {
+                setSelectedWatchList(ele);
+              }}
+            >
+              {ele.watchListName}
+            </div>
+          );
+        })
+      ) : (
+        <select
+          value={selectedCustomOption ? selectedCustomOption.id : ""}
+          onChange={handleSelectChange}
+        >
+          <option className="hidden" value="" disabled>
+            Custom WatchLists
+          </option>
+          {CustomWatchLists &&
+            CustomWatchLists.map((ele) => (
+              <option value={ele.id} key={ele.id} className="w-20 h-20">
+                {ele.watchListName}
+              </option>
+            ))}
+        </select>
+      )}
+    </div>
+  );
+};
 
-export default WatchListsTabs
+export default WatchListsTabs;
