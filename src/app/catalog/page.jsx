23476712118b33cbe3cf5249ea5069e755b4f3aa
Abhay Lodhi <abhaylodhiab11@gmail.com>
2024-11-30 23:19:49 +0530

    stashing ctalog

--- src/app/catalog/page.jsx
+++ src/app/catalog/page.jsx
@@ -3,11 +3,15 @@ import React, { useEffect } from "react";
 import ShowCards from "./components/ShowCards";
 import Filters from "./components/Filters";
 import useAnimeSearchFilterStore from "@/components/ZustandStores/animeSearchFiltersStore";
+import { useRouter } from "next/router";
 
 const Page = () => {
   const { resetFilters } = useAnimeSearchFilterStore();
+  const router = useRouter();
+  const { name } = router.query; // Destructure the specific parameter
 
   useEffect(() => {
+    
     return () => {
       resetFilters();
     };
