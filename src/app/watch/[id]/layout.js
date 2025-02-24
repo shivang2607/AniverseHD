14a31485c771e3a79fb333d8611c661a7f12a685
Shivang <shivangkh26@gmail.com>
2025-02-25 01:36:45 +0530

    monetag script test

---
+++ src/app/watch/[id]/layout.js
@@ -0,0 +1,30 @@
+import Head from "next/head";
+import Script from "next/script";
+
+export default function WatchAnimeLayout({
+  children, // will be a page or nested layout
+}) {
+  return (
+    <>
+      <Head>
+      <Script
+        id="monetag-script"
+        strategy="lazyOnload"
+        async defer
+        dangerouslySetInnerHTML={{
+          __html: `(function(d,z,s){
+              s.src = 'https://' + d + '/401/' + z;
+              try { (document.body || document.documentElement).appendChild(s); } catch(e) { }
+          })('gizokraijaw.net', 8974908, document.createElement('script'));`,
+        }}
+      />
+      </Head>
+
+
+
+      <div>
+        {children}
+      </div>
+    </>
+  )
+}
