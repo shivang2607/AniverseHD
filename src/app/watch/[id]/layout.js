69bad283d1c585175f61be43bdca235ed8897274
Shivang <shivangkh26@gmail.com>
2025-02-25 22:28:29 +0530

    antiaddblock monetag added

--- src/app/watch/[id]/layout.js
+++ src/app/watch/[id]/layout.js
@@ -1,20 +1,36 @@
 import Script from "next/script";
+import { monetagAntiAddBlock } from "./dangerousInnerHTMLContent";
 
 export default function WatchAnimeLayout({
   children, // will be a page or nested layout
 }) {
   return (
     <>
+
+<Script
+        id="monetag-inline-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: monetagAntiAddBlock,
+        }}
+      />
      
      <Script
-        id="monetag-script"
-        strategy="lazyOnload"
-        async defer
+        id="monetag-external-script"
+        strategy="afterInteractive"
         dangerouslySetInnerHTML={{
-          __html: `(function(d,z,s){
-              s.src = 'https://' + d + '/401/' + z;
-              try { (document.body || document.documentElement).appendChild(s); } catch(e) { }
-          })('gizokraijaw.net', 8974908, document.createElement('script'));`,
+          __html: `
+            (function(d, z, s, c){
+              s.src = '//' + d + '/400/' + z;
+              s.onerror = s.onload = E;
+              function E(){ c && c(); c = null }
+              try {
+                (document.body || document.documentElement).appendChild(s)
+              } catch(e) {
+                E()
+              }
+            })('shaiwourtijogno.net', 8974908, document.createElement('script'), _ytcwhw);
+          `,
         }}
       />
 
