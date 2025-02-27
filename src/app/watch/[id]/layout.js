d51811609ad2d728e5ae65e3c68d6c3dd6f27a5c
Shivang <shivangkh26@gmail.com>
2025-02-28 02:05:41 +0530

    push notification removed 2 left

--- src/app/watch/[id]/layout.js
+++ src/app/watch/[id]/layout.js
@@ -1,47 +1,38 @@
 // app/watch/[id]/layout.js
-'use client'
+"use client";
+import { useEffect } from "react";
 import Script from "next/script";
 
-// Generate a timestamp that changes every 20 minutes
+// Generate a timestamp that changes every 10 minutes
 const getCacheBuster = () => {
-  const time = 20 * 60 * 1000;
+  const time = 10 * 60 * 1000;
   return Math.floor(Date.now() / time) * time;
 };
 
 export default function WatchLayout({ children }) {
   const cacheBuster = getCacheBuster();
 
+  useEffect(() => {
+    //script of in page push notification.
+    (function (d, z, s) {
+      s.src = "https://" + d + "/400/" + z;
+      try {
+        (document.body || document.documentElement).appendChild(s);
+      } catch (e) {}
+    })("vemtoutcheeg.com", 9015075, document.createElement("script"));
+  }, []);
+
   return (
     <>
-      <Script
+      {/* <Script
         src={`/scripts/adblocker-detection.js?${cacheBuster}`}
         strategy="afterInteractive"
         data-cfasync="false"
-      />
-
+      /> */}
       <Script
         src={`/scripts/monetag-script.js?${cacheBuster}`}
         strategy="afterInteractive"
       />
-
-      <Script
-        strategy="afterInteractive"
-        src="https://eechicha.com/act/files/tag.min.js?z=9015028"
-        data-cfasync="false"
-        async
-      />
-
-      <Script
-        id="push-notification"
-        strategy="afterInteractive"
-        onLoad={() => {
-          const script = document.createElement("script");
-          script.src = "https://vemtoutcheeg.com/400/9015075";
-          script.async = true;
-          document.body.appendChild(script);
-        }}
-      />
-
       <div>{children}</div>
     </>
   );
