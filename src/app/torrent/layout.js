2c23ca643796427c8e1523d78becec4edb4b6f66
Shivang Khandelwal <85695675+shivang2607@users.noreply.github.com>
2025-05-01 22:29:13 +0530

    Galaksion ads (#22)
    * galaksion button ads integrated on home and watch page
    
    * second commit of this branch

--- src/app/torrent/layout.js
+++ src/app/torrent/layout.js
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import { Suspense } from "react";
 
 
@@ -6,6 +7,7 @@ export default function TorentSearch({
   }) {
     return (
       <div className=" flex w-full gap-4">   
+      <Script data-cfasync="false" async type="text/javascript" src="https://cluckedzion.com/gHBwiL5kU0DqfIaYF/121367"></Script>
         {children}
       </div>
     )
