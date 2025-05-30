600d271bddee5e826c092ca25a8ebc9ad7933bbc
Abhay Lodhi <abhaylodhiab11@gmail.com>
2025-05-30 12:41:32 +0530

    removed ads

--- src/app/torrent/layout.js
+++ src/app/torrent/layout.js
@@ -7,7 +7,6 @@ export default function TorentSearch({
   }) {
     return (
       <div className=" flex w-full gap-4">   
-      <Script data-cfasync="false" async type="text/javascript" src="https://cluckedzion.com/gHBwiL5kU0DqfIaYF/121367"></Script>
         {children}
       </div>
     )
