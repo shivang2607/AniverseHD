b531d3a5f3a4862d9ceb77a8ff28a45b68ffda81
Abhay Lodhi <abhaylodhiab11@gmail.com>
2025-08-26 20:23:22 +0530

    updated exsisting proxies

--- src/app/api/v1/users-cloudflare/utils.js
+++ src/app/api/v1/users-cloudflare/utils.js
@@ -1,7 +1,14 @@
 const API_BASE_URL = `${process.env.WORKER_URL}/api/${process.env.WORKER_VERSION}`;
 
 export async function getUserInfo(userId) {
-    const response = await fetch(`${API_BASE_URL}/user/${userId}`);
+    const response = await fetch(`${API_BASE_URL}/user`,{
+        method: 'GET',
+        headers: {
+        'Content-Type': 'application/json',
+        'user-id': userId,
+        }
+    });
+    
     const data = await response.json();
     return { data, status: response.status };
 }
@@ -11,6 +18,7 @@ export async function addUserInfo(userData) {
         method: 'POST',
         headers: {
         'Content-Type': 'application/json',
+        'user-id': userData.userId,
         },
         body: JSON.stringify(userData),
     });
@@ -19,10 +27,11 @@ export async function addUserInfo(userData) {
 }
 
 export async function updateUser(userId, updateData) {
-    const response = await fetch(`${API_BASE_URL}/user/${userId}`, {
+    const response = await fetch(`${API_BASE_URL}/user`, {
         method: 'PATCH',
         headers: {
         'Content-Type': 'application/json',
+        'user-id': userId,
         },
         body: JSON.stringify(updateData),
     });
